ac45946a2fbe1793a1df782e7ec4ebc8cf3d12b2
xzxiaoshan <365384722@qq.com>
2016-07-31 00:25:43 +0800

    Update TestGit.java

--- src/com/shanhy/demo/TestGit.java
+++ src/com/shanhy/demo/TestGit.java
@@ -7,7 +7,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("测试Git.");
-		System.out.println("陈兴林Hello");
+		System.out.println("陈兴林HelloWorld");
 		System.out.println("xxxxxxxxxxxxYYYYY");
 	}
 }
