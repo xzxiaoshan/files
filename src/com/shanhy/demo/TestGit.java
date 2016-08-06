f9e9e3173cd3565141473d535ce2ed0c13cd1e8f
xzxiaoshan <365384722@qq.com>
2016-08-06 20:19:32 +0800

    update.

--- src/com/shanhy/demo/TestGit.java
+++ src/com/shanhy/demo/TestGit.java
@@ -11,6 +11,7 @@ public class TestGit {
 		System.out.println("xxxxxxxxxxxxYYYYY222222222");
 		System.out.println("new line!!!");
 		System.out.println("new line two");
+		System.out.println("new line three");
 	}
 	
 	public void display(){
